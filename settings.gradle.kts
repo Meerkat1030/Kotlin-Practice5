8b1f1035c63ce7dad2f8f4b4840ec0dc48f399f8
Meerkat1030 <hong991000@gmail.com>
2024-02-13 10:36:09 +0900

    [COMMIT] 02/13 commit Kotlin 연습

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "class20240213"
+include(":app")
+include(":tablayoutex")
---
+++ tablayoutex/src/main/java/com/example/tablayoutex/OneFragment.kt
@@ -0,0 +1,24 @@
+package com.example.tablayoutex
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tablayoutex.R
+import com.example.tablayoutex.databinding.FragmentOneBinding
+
+
+class OneFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+}
